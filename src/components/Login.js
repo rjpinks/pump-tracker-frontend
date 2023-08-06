f566abb4f6532e1005308460917880ba61cf0ec9
Ryan <pinkstonr54@gmail.com>
2023-08-06 00:46:07 -0500

    added functionality to the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = function () {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginHandler = async (event) => {
+        event.preventDefault();
+
+        console.log('email -> ', email);
+        console.log('password -> ', password)
+
+        try {
+            await fetch('http://localhost:3001/api/profile/login', {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+            console.log('Successful Login');
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
     return (
         <form>
             <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="input-email" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                <input
+                    type="email"
+                    className="form-control"
+                    id="input-email"
+                    aria-describedby="emailHelp"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="input-password" />
+                <input
+                    type="password"
+                    className="form-control"
+                    id="input-password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
             </div>
             <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary" onSubmit={loginHandler}>Submit</button>
         </form>
     )
 }
